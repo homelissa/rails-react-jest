8bace27506064122455c4c6b8b5adfa4feedb140
Melissa Ho <mho@guardanthealth.com>
2018-09-27 13:33:38 -0700

    Add MyApp react component and jest test

---
+++ app/javascript/spec/MyApp.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MyApp from '../components/MyApp.js';
+import {shallow} from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+test('CheckboxWithLabel changes the text after click', () => {
+  // Render a checkbox with label in the document
+  Enzyme.configure({ adapter: new Adapter() })
+  const app = shallow(<MyApp />);
+  expect(app.text()).toEqual('Super Guardant');
+  app.find('input').simulate('change');
+  expect(app.text()).toEqual('renamed');
+});
